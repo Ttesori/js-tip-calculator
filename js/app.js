03c0d58a4573c23fa9adb178709c9456c51ba0f3
Toni Tesori <tt@tesoridesign.com>
2022-01-16 14:22:24 -0500

    Basic layout working

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.querySelector('.splitter-percents').addEventListener('change', (e) => {
+
+});
+
+// TODO: Handle clicks on tip amount
+// - Remove selected class on currently selected element
+// // document.querySelector('.selected').classList.remove('selected');
+// - Add selected class to label parentelement
+// //  e.target.parentElement.classList.add('selected');
+// - set current tip amount
+// if bill amount entered, update results
